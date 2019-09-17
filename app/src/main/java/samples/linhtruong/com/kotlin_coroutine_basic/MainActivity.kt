5c7a4dcf8753f93d34a96aa77351f443d09d8202
Linh <Linh@Truong.local>
2019-09-18 01:07:59 +0900

    Add coroutine dependency, add first test job, scope

--- app/src/main/java/samples/linhtruong/com/kotlin_coroutine_basic/MainActivity.kt
+++ app/src/main/java/samples/linhtruong/com/kotlin_coroutine_basic/MainActivity.kt
@@ -1,38 +1,95 @@
 package samples.linhtruong.com.kotlin_coroutine_basic
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "coroutine-test"
+        private const val TIME_1S = 1000L
+        private const val TIME_2S = 1000L
+        private const val TIME_500MS = 500L
+        private const val TIME_200MS = 200L
+        private const val TIME_100MS = 100L
+    }
+
+    private fun logd(msg: String) {
+        Log.d(TAG, msg)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
+        // use runBlocking to for test suspending functions
+//        test1()
+//        test2()
+
+        // join() to wait job to complete
+//        testJob()
+
+        // create custom scope instead
+        testScope()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun test1() {
+        GlobalScope.launch {
+            // launch a new coroutine in background and continue
+            delay(TIME_1S) // non-blocking delay for 1 second (default time unit is ms)
+            logd("World!") // print after delay
+        }
+        logd("Hello,") // main thread continues while coroutine is delayed
+        Thread.sleep(TIME_2S) // block main thread for 2 seconds to keep JVM alive
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+
+    private fun test2() = runBlocking {
+        GlobalScope.launch {
+            // launch a new coroutine in background and continue
+            delay(TIME_1S)
+            logd("World!")
         }
+        logd("Hello,") // main coroutine continues here immediately
+        delay(TIME_2S)      // delaying for 2 seconds to keep JVM alive
+    }
+
+    private fun testJob() = runBlocking {
+        val job = GlobalScope.launch {
+            delay(TIME_1S)
+            logd("World!")
+        }
+
+        logd("Hello,")
+        job.join()
+    }
+
+    private fun testScope() = runBlocking {
+        launch {
+            delay(TIME_200MS)
+            logd("Task from runBlocking scope")
+        }
+
+        coroutineScope {
+            launch {
+                extractLaunchFunc(TIME_500MS)
+                logd("Task from nested launch")
+            }
+
+            delay(TIME_100MS)
+            logd("Task from custom coroutine scope")
+        }
+
+        logd("Coroutine scope is over!")
+    }
+
+    private suspend fun extractLaunchFunc(time: Long) {
+        delay(time)
     }
 }
